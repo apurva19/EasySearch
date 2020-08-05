3d11d4cc1e98c3715717cc6f608668cc1498950c
Apurva Sharma <apurva.sharma.756@gmail.com>
2020-08-06 01:19:00 +0530

    Update popup.js

--- popup.js
+++ popup.js
@@ -31,7 +31,7 @@ function getURL(x, callback){
 getURL("address", function(x){
     urlCurrent = x;
     URLC = x;
-    urlCurrent = "http://easy-search1.herokuapp.com/" + urlCurrent;
+    urlCurrent = "http://flask-javascc.herokuapp.com/" + urlCurrent;
     
     // Extract Keywords from URL
     var keywords = "";
